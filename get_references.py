e1d06b33950b49c9661b98793788806027742730
Jan Leicht <janleicht@MacBook-Pro-4.fritz.box>
2026-03-12 17:06:41 +0100

    NEW: file for recording reference frames

---
+++ get_references.py
@@ -0,0 +1,32 @@
+import cv2
+import time
+
+SAVED_FRAMES_PER_SECOND = 4
+
+if __name__ == "__main__":
+    # get video source (0: switch 2; 1: pc webcam)
+    cap = cv2.VideoCapture(0)
+
+    # set limits for capture of switch 2
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+
+    save_folder = "references/"
+    frame_counter = 0
+
+    # display captured images
+    while True:
+        # capture frame
+        success, frame = cap.read()
+
+        if not success:
+            print("video could not be read")
+            cap.release()
+            cv2.destroyAllWindows()
+            break
+        else:
+            # save frame
+            cv2.imwrite(save_folder + "reference_" + str(frame_counter) + ".png", frame)
+            cv2.waitKey(1)
+            frame_counter += 1
+            time.sleep(1 / SAVED_FRAMES_PER_SECOND)
