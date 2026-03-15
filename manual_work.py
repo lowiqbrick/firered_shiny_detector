409b55ce2a7db4eb95e27437deebe6ac08e9d9d3
Jan Leicht <janleicht@MacBook-Pro-4.fritz.box>
2026-03-15 18:29:36 +0100

    NEW: manual pixel locating function

--- manual_work.py
+++ manual_work.py
@@ -31,14 +31,60 @@ def black_image_part(
     return image
 
 
+def show_pixel_in_image(
+    image: cv2.typing.MatLike, pixel: utils.Point, *args: int
+) -> cv2.typing.MatLike:
+    grid = 0
+    for arg in args:
+        grid = arg
+
+    height, width = image.shape[:2]
+
+    # get pixel
+    assert pixel.x <= width
+    assert pixel.y <= height
+    channel1, channel2, channel3 = (
+        image[pixel.y][pixel.x][0],
+        image[pixel.y][pixel.x][1],
+        image[pixel.y][pixel.x][2],
+    )
+
+    for height_index in range(0, height):
+        for width_index in range(0, width):
+            if height_index == pixel.y or width_index == pixel.x:
+                image[height_index][width_index][0] = 255
+                image[height_index][width_index][1] = 255
+                image[height_index][width_index][2] = 255
+            elif (
+                # grid enabled
+                grid != 0
+                # not at the left or top of the image
+                and (height_index != 0 and width_index != 0)
+                # condition for the grid
+                and ((height_index % grid) == 0 or (width_index % grid) == 0)
+            ):
+                image[height_index][width_index][0] = 122
+                image[height_index][width_index][1] = 122
+                image[height_index][width_index][2] = 122
+            else:
+                image[height_index][width_index][0] = (
+                    image[height_index][width_index][0] / 2 + channel1 / 2
+                )
+                image[height_index][width_index][1] = (
+                    image[height_index][width_index][1] / 2 + channel2 / 2
+                )
+                image[height_index][width_index][2] = (
+                    image[height_index][width_index][2] / 2 + channel3 / 2
+                )
+
+    return image
+
+
 if __name__ == "__main__":
     mewtwo_reference = cv2.imread("selected_references/fight_menue_reference.png")
-    blacked_image = black_image_part(
-        mewtwo_reference[:][:][:],
-        utils.Rectangle(utils.Point(1150, 80), utils.Point(1550, 450)),
-    )
-    cv2.imshow("black example", blacked_image)
+    pixel_emphasised = show_pixel_in_image(mewtwo_reference, utils.Point(1400, 350), 50)
+    cv2.imshow("pixel example", pixel_emphasised)
     cv2.waitKey(1)
 
-    time.sleep(5)
+    time.sleep(10)
     cv2.destroyAllWindows()
