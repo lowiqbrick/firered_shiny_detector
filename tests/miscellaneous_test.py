5c82e450029066454b7d92598b638bc2060b4a8b
Jan Leicht <janleicht@MacBook-Pro-4.fritz.box>
2026-04-06 10:45:29 +0200

    NEW: color channel test

--- tests/miscellaneous_test.py
+++ tests/miscellaneous_test.py
@@ -1,3 +1,4 @@
+import cv2
 import utils
 
 
@@ -5,3 +6,34 @@ def test_period_to_string_length():
     assert len(utils.period_to_str(12.12)) == 5
     assert len(utils.period_to_str(12.1)) == 5
     assert len(utils.period_to_str(12)) == 5
+
+
+def test_cv2_color_channels():
+    """
+    test image generated via:
+
+    import cv2
+    import numpy as np
+
+    cholorchannel_test = np.zeros((1080, 1920, 3)) + 128
+
+    for height_index in range(0, 1080):
+        for width_index in range(0, 1920):
+            if height_index <= 510 and width_index <= 990:
+                cholorchannel_test[height_index][width_index][0] = 255
+            if height_index > 510 and width_index < 990:
+                cholorchannel_test[height_index][width_index][1] = 255
+            if height_index < 510 and width_index > 990:
+                cholorchannel_test[height_index][width_index][2] = 255
+    cv2.imwrite("selected_references/cholor_channel_reference.png", cholorchannel_test)
+    """
+
+    test_image = cv2.imread("selected_references/cholor_channel_reference.png")
+
+    # image dimensions (height, width, color dims) (1020, 1980, 3)
+    # top left blue
+    assert test_image[0][0][0] == 255
+    # bottom left green
+    assert test_image[1000][0][1] == 255
+    # top right red
+    assert test_image[0][1100][2] == 255
